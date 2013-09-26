2917ee40886e9bdb5d9b6f1a8c27db442def99b5
sv90 <sandervervaart@MacBook-Pro-van-Sander-Vervaart.local>
2013-09-26 08:49:07 +0200

    Deeltoets 1
    Score win aangepast

--- Mijn deeltoets 1/static/js/app.js
+++ Mijn deeltoets 1/static/js/app.js
@@ -1,6 +1,9 @@
+// Namespace
 var FRISBEE = FRISBEE || {};
 
+//Empty function
 (function () {
+	
 	// Data objecten
 	FRISBEE.schedule = {
 
@@ -48,9 +51,9 @@ var FRISBEE = FRISBEE || {};
 
 	FRISBEE.ranking = {
 		rankings: [
+	{ team: "Burning Snow", Win: "3", Lost: "1", Sw: "11", Sl: "4", Pw: "36", Pl: "23"},
+	{ team: "Boomsquad", Win: "2", Lost: "2", Sw: "9", Sl: "8", Pw: "36", Pl: "34"},
 	{ team: "Chasing", Win: "2", Lost: "2", Sw: "7", Sl: "9", Pw: "35", Pl: "39"},
-    { team: "Boomsquad", Win: "2", Lost: "2", Sw: "9", Sl: "8", Pw: "36", Pl: "34"},
-    { team: "Burning Snow", Win: "3", Lost: "1", Sw: "11", Sl: "4", Pw: "36", Pl: "23"},
     { team: "Beast Amsterdam", Win: "2", Lost: "2", Sw: "6", Sl: "8", Pw: "30", Pl: "34"},
     { team: "Amsterdam Money Gang", Win: "1", Lost: "3", Sw: "6", Sl: "10", Pw: "30", Pl: "37"}]
 	};
