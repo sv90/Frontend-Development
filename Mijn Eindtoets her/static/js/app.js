0659bdb7d3ddfd053b948b17b8617fe6ec004713
sv90 <sandervervaart@MacBook-Pro-van-Sander-Vervaart.local>
2013-11-25 13:00:47 +0100

    -

--- Mijn Eindtoets her/static/js/app.js
+++ Mijn Eindtoets her/static/js/app.js
@@ -1,3 +1,34 @@
+    /* Technische breakdown 
+        - FRISBEE
+                - startApp
+                        - init()
+                                - router.init()
+                                - gestures.init()
+                                - game.init()
+                - router
+                        - init ()
+                                - game: game_id
+                                - schedule
+                                - Ranking
+                                - *
+                - gestures
+                        - init()
+                - game
+                        - init()
+                                - FetchAll()
+                                - Post(url,postData)
+                                        - page.schedule()
+                - directives
+                        - schedule
+                - page
+                        - game: game_id
+                        - schedule
+                        - Ranking
+                                - Get(url)
+                                - render
+                - Loader
+    */
+
 // Global namespace
 var FRISBEE = FRISBEE || {};
 
@@ -6,7 +37,7 @@ var FRISBEE = FRISBEE || {};
     // Returns HTML class element named bubblingG, which contains the loader
     var loader = document.getElementsByClassName('bubblingG')[0];
 
-    // Start, render application when DOM is ready // Execute init method's inside router, gestures and game
+    // Start, render application when DOM is ready // Execute init method's inside 'router', 'gestures' and 'game'
     FRISBEE.startApp = {
         init: function(){
             FRISBEE.router.init();
@@ -40,9 +71,9 @@ var FRISBEE = FRISBEE || {};
             });
         },
 
-        change: function (pageName) {
+        change: function (router) {
         // Qwery is a Javascript engine to select DOM elements
-            var route = pageName,
+            var route = router,
                 sections = qwery('section[data-route]'),
                 section = qwery('[data-route=' + route + ']')[0];  
 
@@ -51,7 +82,7 @@ var FRISBEE = FRISBEE || {};
                 for (var i=0; i < sections.length; i++){
                     sections[i].classList.remove('active');
                 }
-                $$('[data-route='+pageName+']')[0].classList.add('active');
+                $$('[data-route='+router+']')[0].classList.add('active');
             }
 
             // Default route (no route, go default state)
@@ -82,41 +113,63 @@ var FRISBEE = FRISBEE || {};
             document.getElementById('finalScore').onclick = function () {
                 var type =  'POST',
                 url  =  'https://api.leaguevine.com/v1/game_scores/',
-                // Convert value to JSON
-                postData         = JSON.stringify({
-                    game_id: FRISBEE.page.game.scoreData.id,
-                    team_1_score: FRISBEE.game.getScore1(),
-                    team_2_score: FRISBEE.game.getScore2(),
-                    is_final: 'True'
-                });
+                    // Convert value to JSON
+                    uploadScore = JSON.stringify({
+                        game_id: FRISBEE.page.game.scoreData.id,
+                        team_1_score: FRISBEE.game.getScore1(),
+                        team_2_score: FRISBEE.game.getScore2(),
+                        is_final: 'True'
+                    });
 
             // Create request
             var xhr = new XMLHttpRequest();
 
-            // Open request
+            // Open request, asynchronous (synchronous blocks the execution of code)
             xhr.open(type,url,true);
 
-            // Set request headers
+            // Set request headers, adds HTTP headers to the request (header, value)
             xhr.setRequestHeader('Content-type','application/json');
             xhr.setRequestHeader('Authorization','bearer 82996312dc');
 
             // Send request (with data as a json string)
-            xhr.send(postData);
-            console.log("verzonden");
+            xhr.send(uploadScore);
+            
+            // onreadystatechange stores a function to be called automatically each time the readyState property changes
+            // readyState holds the status of XMLHttpRequest
+            // Check for response: 4 = request finished and response is ready
+            // 200 = OK, in case of Leaguevine's API it is 201
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState == 4 && xhr.status == 201) {
+                    FRISBEE.page.schedule();
+                }
+            };
         }
         
-            // Math function
+            // Math function, parseInt parses a string argument and returns an integer of the specified radix or base
             score1 = parseInt(document.getElementById('addScore1').innerHTML);
             score2 = parseInt(document.getElementById('addScore2').innerHTML);
 
+            // onclick: Event occurs when user clicks on an element
             document.getElementById('plus1').onclick = function () {
                 score1++;
+                document.getElementById('addScore1').innerHTML = score1;
+                console.log(score1);
+            };
+
+            document.getElementById('min1').onclick = function () {
+                score1--;
                 document.getElementById('addScore1').innerHTML = score1;
                 console.log(score1);
             };
     
             document.getElementById('plus2').onclick = function () {
                 score2++;
+                document.getElementById('addScore2').innerHTML = score2;
+                console.log(score2);
+            };
+
+            document.getElementById('min2').onclick = function () {
+                score2--;
                 document.getElementById('addScore2').innerHTML = score2;
                 console.log(score2);
             };
@@ -139,6 +192,7 @@ var FRISBEE = FRISBEE || {};
     }
 
     // Two-dimensional object literal, performed by Transparency while rendering the template
+    // Als track_score in de code wordt aangeroepen - return gamepagina + game_id
     FRISBEE.directives = {
         schedule: {
             objects: {
@@ -164,25 +218,22 @@ var FRISBEE = FRISBEE || {};
                 console.log(FRISBEE.page.game.scoreData);
                 //Hide the loader
                 loader.style.display = 'none';
+                FRISBEE.router.change('game');
                 });
-
-            FRISBEE.router.change('game');
         },
 
         schedule: function () {
             //Show the loader
             loader.style.display = 'block';
             $$.get('https://api.leaguevine.com/v1/games/?pool_id=19219&access_token=1402975c1b',{}, function(data){
-                document.getElementById('schedule-pool');
                 var directives = FRISBEE.directives.schedule;
                 FRISBEE.page.schedule.poolData = data;
                 Transparency.render(qwery('[data-route=schedule]')[0], FRISBEE.page.schedule.poolData, directives);
                 console.log(FRISBEE.page.schedule.poolData);
                 //Hide the loader
                 loader.style.display = 'none';
+                FRISBEE.router.change('schedule');
                 });
-
-            FRISBEE.router.change('schedule');
         },
 
         Ranking: function () {
@@ -192,9 +243,8 @@ var FRISBEE = FRISBEE || {};
                 Transparency.render(qwery('[data-route=Ranking]')[0], data);
                 //Hide the loader
                 loader.style.display = 'none';
+                FRISBEE.router.change('Ranking');
                 });
-
-            FRISBEE.router.change('Ranking');
         }
     }
 
